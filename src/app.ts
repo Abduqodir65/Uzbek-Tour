2124ed25e3d4af68c46a8eb4f4de352e83e8bbe2
Abduqodir65 <abduqodirgithub@gmail.com>
2024-09-30 16:33:07 +0500

    app.ts togirlandi

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,43 @@
 import { Module } from '@nestjs/common';
-import { appConfig, dbConfig } from './config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig, dbConfig } from '@config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { City, Galery, Routes } from './modules';
-
+import { City, CityModule, Galery, GaleryModule, Routes } from './modules';
+import { RouterModule } from '@nestjs/core';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, dbConfig]
+      load: [appConfig, dbConfig],
     }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        dialect: "postgres",
-        host: config.get('database.host'),
-        port: +config.get('database.port'),
-        username: config.get('database.user'),
-        password: config.get('database.password'),
-        database: config.get('database.dbName'),
-        models: [City,Routes,Galery],
-        synchronize: true,
-      }),
-      inject: [ConfigService]
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        try {
+          return {
+            dialect: 'postgres',
+            host: config.get('database.host'),
+            port: config.get<number>('database.port'),
+            username: config.get('database.user'),
+            password: config.get('database.password'),
+            database: config.get('database.dbName'),
+            models: [City,Routes,Galery],
+            synchronize: true,
+            sync: { force: true },
+            logging: console.log,
+            autoLoadModels: true,
+          };
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }),
+    CityModule,
+    RouterModule,
+    GaleryModule
   ],
 })
 export class AppModule {}
