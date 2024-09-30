e1191bffc013a06edb2db81793392276c23387d1
Abduqodir65 <abduqodirgithub@gmail.com>
2024-09-30 16:51:33 +0500

    user schema qoshildi

---
+++ src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,19 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({ timestamps: true, tableName: 'users' })
+export class User extends Model {
+    @Column({ allowNull: false, type: DataType.STRING })
+    name: string;
+
+    @Column({ type: DataType.INTEGER, allowNull: false })
+    age: number;
+
+    @Column({ type: DataType.TEXT, allowNull: false })
+    country: string;
+
+    @Column({ type: DataType.TEXT, allowNull: false })
+    email: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    image: string;
+}
