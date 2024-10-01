71b5c6f108ceb03de3f39287bc09bf33b7d0969c
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-01 14:20:05 +0500

    multer config qoshildi

---
+++ src/config/multer.config.ts
@@ -0,0 +1,15 @@
+import multer from "multer";
+import path from "path";
+
+export const multerConfig = {
+    storage: multer.diskStorage({
+        destination(req,file, callback) {
+            return callback(null, './uploads')
+        },
+        filename: function (req, file, cb) {
+            const extname = path.extname(file.originalname);
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+            cb(null, file.fieldname + '-' + uniqueSuffix + extname)
+        }
+    })
+}
