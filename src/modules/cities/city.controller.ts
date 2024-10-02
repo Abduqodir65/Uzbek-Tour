8e4af8ca6abed5f4c994e20add934da49ce0914f
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-02 12:28:58 +0500

    city modelga crud qilindi

--- src/modules/cities/city.controller.ts
+++ src/modules/cities/city.controller.ts
@@ -1,23 +1,71 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { CityService } from "./city.service";
 import { City } from "./schemas";
+import { CreateCityRequest } from "./interface";
+import { FileInterceptor } from "@nestjs/platform-express";
+import * as multer from "multer";
+import * as path from "path";
 
 @Controller('cities')
 export class CityController {
     #_service: CityService;
 
-    constructor(service: CityService){
+    constructor(service: CityService) {
         this.#_service = service
     }
-    
+
     @Get()
-    async getCities() :Promise<City[]> {
+    async getCities(): Promise<City[]> {
         return await this.#_service.getAllCities();
     }
 
     @Get('/id')
-    async getSingleCity(id:number): Promise<City> {
+    async getSingleCity(id: number): Promise<City> {
         return await this.#_service.getSingleCity(id);
+    }
+
+    @Post('/add')
+    @UseInterceptors(FileInterceptor("image", {
+        storage: multer.diskStorage({
+            destination(req, file, callback) {
+                return callback(null, "./uploads");
+            },
+            filename(req, file, cb) {
+                const extName = path.extname(file.originalname);
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+                cb(null, file.fieldname + '-' + uniqueSuffix + extName);
+            }
+        })
+    }))
+    async createCity(
+        @Body() createCityPayload: CreateCityRequest,
+        @UploadedFile() image: Express.Multer.File
+    ): Promise<{ message: string; city: CreateCityRequest }> {
+        if (image) {
+            createCityPayload.image = image.filename;
+        }
+        await this.#_service.createCity(createCityPayload);
+        return {
+            message: 'success',
+            city: createCityPayload
+        };
+    }
+
+    @Put('/update/:id')
+    async updateCity(
+        @Param('id') id: number,
+        @Body() updateCityPayload: CreateCityRequest
+    ): Promise<{ message: string; city: City }> {
+        const updatedCity = await this.#_service.updateCity(id, updateCityPayload);
+        return {
+            message: 'City updated successfully',
+            city: updatedCity.city
+        };
+    }
+
+    @Delete('/delete/:id')
+    async deleteCity(@Param('id') id: number): Promise<{ message: string }> {
+        return await this.#_service.deleteCity(id);
     }
 
 }
--- src/modules/cities/city.service.ts
+++ src/modules/cities/city.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { City } from "./schemas";
 import { InjectModel } from "@nestjs/sequelize";
+import { CreateCityRequest } from "./interface";
+import * as fs from 'fs/promises'
+import * as path from "path";
 
 @Injectable()
 export class CityService {
-    constructor(@InjectModel(City) private cityModel: typeof City) {}
+    constructor(@InjectModel(City) private cityModel: typeof City) { }
 
     async getAllCities(): Promise<City[]> {
         return await this.cityModel.findAll();
@@ -12,9 +15,50 @@ export class CityService {
 
     async getSingleCity(id: number): Promise<City> {
         return await this.cityModel.findOne({
-            where:{id}
+            where: { id }
         })
     }
 
-    
+    async createCity(payload: CreateCityRequest): Promise<{ message: string; city: City }> {
+        const city = await this.cityModel.create({
+            name: payload.name,
+            description: payload,
+            region: payload.region,
+            history: payload.history,
+            climate: payload.climate,
+            image: payload.image,
+            video: payload.video
+
+        });
+        return { message: 'City created successfully', city };
+    }
+
+    async updateCity(id: number, payload: CreateCityRequest): Promise<{ message: string; city: City }> {
+        const city = await this.cityModel.findOne({ where: { id } });
+        if (!city) {
+            throw new Error('City not found');
+        }
+
+        await city.update(payload);
+        return { message: 'City updated successfully', city };
+    }
+
+    async deleteCity(id: number): Promise<{ message: string }> {
+        const city = await this.cityModel.findOne({ where: { id } });
+        if (!city) {
+            throw new Error('City not found');
+        }
+
+        if (city.city_image) {
+            const imagePath = path.join(__dirname, '..', 'uploads', city.city_image);
+            try {
+                fs.unlink(imagePath);
+            } catch (err) {
+                console.error('Error while deleting image file:', err);
+                throw new Error('Failed to delete image file');
+            }
+        }
+        await city.destroy();
+        return { message: 'City deleted successfully' };
+    }
 }
