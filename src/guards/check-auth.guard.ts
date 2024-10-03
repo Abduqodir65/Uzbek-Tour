bed0edefd141dbeb7418b87b77956acd5e1eca5a
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-03 14:55:27 +0500

    .

--- src/guards/check-auth.guard.ts
+++ src/guards/check-auth.guard.ts
@@ -1,12 +1,14 @@
 import { BadRequestException, CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { Observable } from "rxjs";
 import { Protected } from "src/decorators/protected.decorator";
 
 @Injectable()
 export class CheckAuthGuard implements CanActivate {
-    constructor(private reflector: Reflector) {}
+    constructor(private reflector: Reflector, private jwtService:JwtService,private configService:ConfigService) {}
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const ctx = context.switchToHttp();
         const request = ctx.getRequest<Request>()
@@ -21,6 +23,9 @@ export class CheckAuthGuard implements CanActivate {
         }
 
         const token = bearerToken.split("Bearer ")[1]
+        
+
+        this.jwtService.verify(token, {secret:this.configService.get<string>('jwt.accessKey')})
         
         return true;
     }
