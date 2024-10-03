a8d90627ef50e04618f2b712b1e42356ce5b66fb
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-03 14:47:42 +0500

    jwt config qoshildi

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 export * from './app.config'
 export * from './database.config'
+export * from './multer.config'
+export * from './jwt.config'
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,10 @@
+import { access } from "fs";
+
+export const jwtConfig = () => ({
+    jwt: {
+        accessKey: process.env.ACCESS_TOKEN_SECRET_KEY,
+        accessTime: parseInt(process.env.ACCESS_TOKEN_EXPIRE_TIME),
+        refreshKey: process.env.REFRESH_TOKEN_SECRET_KEY,
+        refreshTime: process.env.REFRESH_TOKEN_EXPIRE_TIME
+    }
+})
