a363bbd9c8d7ca5fb16929b4194357932635f903
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-08 15:11:02 +0500

    bla bla

--- src/client/bot/bot.service.ts
+++ src/client/bot/bot.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { Ctx, Start,  Update } from "nestjs-telegraf";
+import { Command, Ctx, Start,  Update } from "nestjs-telegraf";
 import { Context } from "telegraf";
 
 @Injectable()
@@ -7,6 +7,14 @@ import { Context } from "telegraf";
 export class BotService {
     @Start()
     async startBot(@Ctx() context: Context) {
-        context.reply("Assalomu alekum!\n Uzbek-Tour sayting botiga hush kelibsiz'😊")
+        context.reply("Assalomu alekum!\nUzbek-Tour sayting botiga hush kelibsiz😊")
+    }
+
+    @Command("help")
+    async helpCommand(@Ctx() context:Context) : Promise<void>{
+        context.replyWithHTML(`<b>Botdagi kommandalar:</b>
+<i>/start - botni ishga tushirish</i>
+<i>/help - botni kommandalarini ko'rish</i>
+                `)
     }
 }
